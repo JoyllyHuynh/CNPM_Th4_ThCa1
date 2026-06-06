e8664b7e9bfb08f18df41d97998e386002ac15d7
Tran Thi Minh Thu <23130318@st.hcmuaf.edu.vn>
2026-06-06 23:31:38 +0700

    [FIX]: Sửa lỗi conflict

--- WebLuuTruHinhAnh/src/main/java/DAO/UserDao.java
+++ WebLuuTruHinhAnh/src/main/java/DAO/UserDao.java
@@ -101,22 +101,7 @@ public class UserDao extends BaseDao {
         );
     }
 
-    public User getUserById(int id) {
 
-        String sql = """
-        SELECT *
-        FROM users
-        WHERE id = :id
-        """;
-
-        return getJdbi().withHandle(handle ->
-                handle.createQuery(sql)
-                        .bind("id", id)
-                        .mapToBean(User.class)
-                        .findOne()
-                        .orElse(null)
-        );
-    }
 
     public boolean updateProfile(
             int userId,
